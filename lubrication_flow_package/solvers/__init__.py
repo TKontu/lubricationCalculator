5916038d9ae3f8201ab0b1ac8e874499503eba83
Tuomo Vendelin <tuomo_kon@hotmail.com>
2025-06-12 22:22:41 +0300

    refactoring12

--- lubrication_flow_package/solvers/__init__.py
+++ lubrication_flow_package/solvers/__init__.py
@@ -3,7 +3,8 @@ Solvers subpackage - Network flow solving algorithms
 """
 
 from .config import SolverConfig
-from .network_flow_solver import NetworkFlowSolver
+#from .BAKBAKBAK_network_flow_solver import NetworkFlowSolver
 from .nodal_matrix_solver import NodalMatrixSolver
 
-__all__ = ['SolverConfig', 'NetworkFlowSolver', 'NodalMatrixSolver']
+#__all__ = ['SolverConfig', 'NetworkFlowSolver', 'NodalMatrixSolver']
+__all__ = ['SolverConfig', 'NodalMatrixSolver']
